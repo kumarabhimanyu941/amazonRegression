b043d87b48423fc8e6009ab262ffdb671c65d6f1
Abhimanyu <kumarabhimanyu941@gmail.com>
2024-05-07 11:42:11 +0530

    added the configuration for Allure report

--- src/main/java/com/qa/amazon/base/Base.java
+++ src/main/java/com/qa/amazon/base/Base.java
@@ -11,10 +11,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
